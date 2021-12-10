06d42584ada4e5f2eb561ea64127bdfd1c25b0c8
harshitch09 <83729071+harshitch09@users.noreply.github.com>
2021-12-10 11:59:14 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,32 @@
+function initial()
+{
+    setTimeout(function()
+    {
+        document.getElementById("logo").style.opacity = '1';
+    },700);
+    setTimeout(function()
+    {
+        document.getElementById("head").style.opacity = '1';
+    },1100);
+
+    setTimeout(function()
+    {
+        document.getElementById("content").style.opacity = '1';
+    },1100);
+
+    setTimeout(function()
+    {
+        document.getElementById("end_link").style.opacity = '1';
+    },1100);
+
+    setTimeout(function()
+    {
+        document.getElementById("footer").style.opacity = '1';
+    },1500);
+
+    setTimeout(function()
+    {
+        document.getElementById("dot1").style.opacity = '1';
+        document.getElementById("dot2").style.opacity = '1';
+    },900);
+}
